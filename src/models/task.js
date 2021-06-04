851b4e59633d2658aa791e72b5a74dc3837c8666
Mohamed Mohiey <mohhiey@gmail.com>
2021-06-04 17:30:43 +0200

    enbaled timestamps forboth user and task schemas and added filtering tasks by completeion

--- src/models/task.js
+++ src/models/task.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const taskSchema = new mongoose.Schema({
-  description: { type: String, trim: true, required: true },
-  completed: { type: Boolean, default: false },
-  user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-});
+const taskSchema = new mongoose.Schema(
+  {
+    description: { type: String, trim: true, required: true },
+    completed: { type: Boolean, default: false },
+    user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+  },
+  { timestamps: true }
+);
 
 taskSchema.pre("save", async function (next) {
   const task = this;
--- src/models/user.js
+++ src/models/user.js
@@ -5,37 +5,40 @@ const { jwtSecretKey } = require("../../keys");
 const jwt = require("jsonwebtoken");
 const Task = require("./task");
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, trim: true, required: true },
-  email: {
-    type: String,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    required: true,
-    validate: (email) => {
-      if (!validator.isEmail(email)) throw new Error("Email is invalid");
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, trim: true, required: true },
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      required: true,
+      validate: (email) => {
+        if (!validator.isEmail(email)) throw new Error("Email is invalid");
+      },
     },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate: (age) => {
-      if (age < 0) throw new Error("Age must be greater than zero");
+    age: {
+      type: Number,
+      default: 0,
+      validate: (age) => {
+        if (age < 0) throw new Error("Age must be greater than zero");
+      },
     },
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minLength: 8,
-    validate: (password) => {
-      if (password.toLowerCase().includes("password"))
-        throw new Error(`Password can't include "passowrd" word`);
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 8,
+      validate: (password) => {
+        if (password.toLowerCase().includes("password"))
+          throw new Error(`Password can't include "passowrd" word`);
+      },
     },
+    tokens: [{ token: { type: String, required: true } }],
   },
-  tokens: [{ token: { type: String, required: true } }],
-});
+  { timestamps: true }
+);
 
 userSchema.virtual("tasks", {
   ref: "Task",
