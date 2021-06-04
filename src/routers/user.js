b16076eaf77901b310cb218d90ecfe3e42ccbb27
Mohamed Mohiey <mohhiey@gmail.com>
2021-06-05 00:02:26 +0200

    added serving the avatar endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -141,6 +141,17 @@ router.delete("/users/me/avatar", [auth], async (req, res) => {
   res.send(req.user);
 });
 
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user || !user.avatar) throw new Error();
+    res.set("Content-Type", "image/jpg");
+    res.send(user.avatar);
+  } catch (e) {
+    res.status(404).send();
+  }
+});
+
 router.delete("/users/me", [auth], async (req, res) => {
   const _id = req.params.id;
 
